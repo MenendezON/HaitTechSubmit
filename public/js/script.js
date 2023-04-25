ebdf97510833f247568dbc818ebc55da8d80876c
MenendezON <menendezon@gmail.com>
2023-04-25 21:12:04 +0000

    Complete Home page for mobile and desktop version

---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+
+
+
+const para = document.createElement("button");
+para.innerText = "Join CC Global Summit 2023";
+para.classList.add('btn');
+const spaceBtn = document.getElementById('program');
+spaceBtn.appendChild(para);
