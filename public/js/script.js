faae3f2e94db6b05d35f365919271be81551c84f
MenendezON <menendezon@gmail.com>
2023-04-26 16:30:05 +0000

    Update script.js file

--- public/js/script.js
+++ public/js/script.js
@@ -9,47 +9,46 @@ spaceBtn.appendChild(para);
 
 // Generate the featured section
 const presenters = [
-    {
-        title: 'Barak OBAMA',
-        subtitle: 'Form president of the United state of america (USA)',
-        avatar: './public/images/barak_obama.jpg',
-        caption: 'Benkler studies commons-based peer production, and published his seminal book the Wealth of Networks in 2006.'
-    },
-    {
-        title: 'Marissa SONG',
-        subtitle: 'Director of Art Centre Nabi and a board member of CC',
-        avatar: './public/images/marissa_song.jpg',
-        caption: 'Benkler studies commons-based peer production, and published his seminal book the Wealth of Networks in 2006.'
-    },
-    {
-        title: 'Elon MUSK',
-        subtitle: 'CEO of Starlink and Tesla',
-        avatar: './public/images/elon_musk.jpg',
-        caption: 'Benkler studies commons-based peer production, and published his seminal book the Wealth of Networks in 2006.'
-    },
-    {
-        title: 'Bill GATES',
-        subtitle: 'CEO of Microsoft Windows',
-        avatar: './public/images/bill_gates.jpg',
-        caption: 'Benkler studies commons-based peer production, and published his seminal book the Wealth of Networks in 2006.'
-    },
-    {
-        title: 'Susan Wojcicki',
-        subtitle: 'Chairwoman at Cambridge University',
-        avatar: './public/images/susan_wojcicki.jpg',
-        caption: 'Benkler studies commons-based peer production, and published his seminal book the Wealth of Networks in 2006.'
-    },
-    {
-        title: 'Pia OLSON',
-        subtitle: 'Moderator',
-        avatar: './public/images/pia_olson.png',
-        caption: 'Benkler studies commons-based peer production, and published his seminal book the Wealth of Networks in 2006.'
-    },
+  {
+    title: 'Barak OBAMA',
+    subtitle: 'Form president of the United state of america (USA)',
+    avatar: './public/images/barak_obama.jpg',
+    caption: 'Benkler studies commons-based peer production, and published his seminal book the Wealth of Networks in 2006.',
+  },
+  {
+    title: 'Marissa SONG',
+    subtitle: 'Director of Art Centre Nabi and a board member of CC',
+    avatar: './public/images/marissa_song.jpg',
+    caption: 'Benkler studies commons-based peer production, and published his seminal book the Wealth of Networks in 2006.',
+  },
+  {
+    title: 'Elon MUSK',
+    subtitle: 'CEO of Starlink and Tesla',
+    avatar: './public/images/elon_musk.jpg',
+    caption: 'Benkler studies commons-based peer production, and published his seminal book the Wealth of Networks in 2006.',
+  },
+  {
+    title: 'Bill GATES',
+    subtitle: 'CEO of Microsoft Windows',
+    avatar: './public/images/bill_gates.jpg',
+    caption: 'Benkler studies commons-based peer production, and published his seminal book the Wealth of Networks in 2006.',
+  },
+  {
+    title: 'Susan Wojcicki',
+    subtitle: 'Chairwoman at Cambridge University',
+    avatar: './public/images/susan_wojcicki.jpg',
+    caption: 'Benkler studies commons-based peer production, and published his seminal book the Wealth of Networks in 2006.',
+  },
+  {
+    title: 'Pia OLSON',
+    subtitle: 'Moderator',
+    avatar: './public/images/pia_olson.png',
+    caption: 'Benkler studies commons-based peer production, and published his seminal book the Wealth of Networks in 2006.',
+  },
 ];
 
-
 const showCards = (i) => {
-    featured.innerHTML += `<div class="card">
+  featured.innerHTML += `<div class="card">
     <img src="${presenters[i].avatar}" alt=" ">
     <h2>${presenters[i].title}</h2>
     <em>${presenters[i].subtitle}</em>
@@ -59,10 +58,7 @@ const showCards = (i) => {
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-    presenters.forEach((ele, i) => {
-        if (i >= 0) showCards(i);
-    });
+  presenters.forEach((ele, i) => {
+    if (i >= 0) showCards(i);
+  });
 });
-
-
-
